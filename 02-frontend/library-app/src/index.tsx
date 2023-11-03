f98c972a1c7fcb683812e5e9c51286d5b1160319
neoloser23 <76801824+neoloser23@users.noreply.github.com>
2023-11-03 15:23:49 +0530

    Revert "Added notification feature"

--- 02-frontend/library-app/src/index.tsx
+++ 02-frontend/library-app/src/index.tsx
@@ -3,31 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import {App} from './App';
 import {BrowserRouter} from  'react-router-dom';
-import { ToastContainer } from 'react-toastify';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <BrowserRouter>
-<ToastContainer
-          position="top-center"
-          autoClose={1000}
-          limit={2}
-          hideProgressBar={false}
-          newestOnTop={false}
-          closeOnClick
-          rtl={false}
-          pauseOnFocusLoss
-          draggable
-          pauseOnHover
-          theme="light"
-/>
     <App />
-
   </BrowserRouter>
   
-
 );
 
 
